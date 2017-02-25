8f731e18bba602e5c3c7681b56411989840ac7e9
nlread <nedlyfordread@gmail.com>
2017-02-24 20:18:05 -0600

    Basic socket test working and server-app-comm diagram

--- server/app.js
+++ server/app.js
@@ -3,14 +3,28 @@ console.log('Deploying server...');
 //Load Dependencies
 let http = require('http');
 let express = require('express');
+let socketIO = require('socket.io');
 
+//Setup server and IO objects
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use('/', express.static(__dirname + "/public"));
+io.on('connection', function(socket) {
+    console.log('connection created');
+    console.log(socket.id);
 
-let server = app.listen(3000, function() {
-    let host = server.address().address;
-    let port = server.address().port;
+    socket.on('dog', function() {
+        console.log('dog recieved');
+    });
+})
+server.listen(3000);
 
-    console.log('App listening at http://%s:%s', host, port);
-});
+
+// let server = app.listen(3000, function() {
+//     let host = server.address().address;
+//     let port = server.address().port;
+
+//     console.log('App listening at http://%s:%s', host, port);
+// });
--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,9 +1,35 @@
 function setup() {
     $('#header').click(youClickedDog);
+    $('#btnConnect').click(testConnect);
+    $('#btnSendDog').click(sendDog);
+    $('#btnSendCat').click(sendCat);
 }
 
+let socket;
 function youClickedDog() {
     alert('YOU CLICKED ON THE DOG!!!');
+}
+
+function testConnect() {
+    console.log('establishing socket connection');
+    socket = io.connect();
+    socket.emit('dog');
+}
+
+function sendDog() {
+    if(socket === undefined) {
+        console.log('error no connection');
+    } else {
+        socket.emit('dog');
+    }
+}
+
+function sendCat() {
+    if(socket === undefined) {
+        console.log('error no connection');
+    } else {
+        socket.emit('cat lol');
+    }
 }
 
 window.onload = setup;
